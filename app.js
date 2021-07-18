97976ab0b2a699b1b1d4bfe1f1f1b8a630fb0e13
kevinmalan <kmalan@outlook.com>
2021-07-18 14:38:21 +0200

    Added word pairs

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
+        words: '',
         wordCount: 0,
-        words: ''
+        wordPairs: {}
     },
     methods: {
         analyzeWords: function () {
             var wordArr = getWordArray(this.words);
             this.wordCount = getWordCount(wordArr);
+            this.wordPairs = getWordPairs(wordArr);
         }
     }
   })
@@ -20,4 +22,33 @@ function getWordArray(words) {
 
 function getWordCount(wordArr) {
     return wordArr.length;
+}
+
+function getWordPairs(wordArr) {
+   var wordPairs = {};
+   wordArr.forEach(word => {
+       if (word in wordPairs) {
+           wordPairs[word] += 1;
+       }
+       else 
+       {
+           wordPairs[word] = 1;
+       }
+   });
+
+   return sortObjectByValueDescending(wordPairs);
+}
+
+function sortObjectByValueDescending(obj) {
+    var keysSortedByValueArr = Object.keys(obj).sort(function (a, b) {
+        return obj[b] - obj[a];
+   });
+
+   var objSorted = {};
+
+   for (key of keysSortedByValueArr) {
+    objSorted[key] = obj[key];
+   }
+
+   return objSorted;
 }
