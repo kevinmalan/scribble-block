58d773bf064c6ed14f89b04c50179d1802c1c5a8
kevinmalan <kmalan@outlook.com>
2021-07-24 22:27:21 +0200

    Added more flexible word pair rendering

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = new Vue({
     methods: {
         analyzeWords: function () {
             var words = this.words.replace("\n", " ").replace("\t", " ");
-            var wordArr = getWordArray(words.replace(/[.,\/#!$?@#^%\^&\*;:{}=\-_`~()]/g,""));
+            var wordArr = getWordArray(words.replace(/[.,\/#!$?@#^%\^&\*;:{}=\-_`~()]/g, ""));
             this.wordCount = getWordCount(wordArr);
             this.wordPairs = getWordPairs(wordArr);
         },
@@ -24,13 +24,13 @@ var app = new Vue({
             this.words = window.localStorage.getItem('words-1');
             this.analyzeWords();
         },
-        highlight: function(word) {
+        highlight: function (word) {
             var textarea = document.getElementById("words");
             var index = -1;
 
             if (this.highlighted.previousWord === word) {
                 var subString = textarea.value.substring(this.highlighted.indexAfterWord);
-                index =  subString.indexOf(word);
+                index = subString.indexOf(word);
                 if (index != -1) {
                     index += this.highlighted.indexAfterWord;
                 }
@@ -39,7 +39,7 @@ var app = new Vue({
             if (index == -1) {
                 index = textarea.value.indexOf(word);
             }
-            
+
             var indexAfterWord = index + word.length;
             textarea.focus();
             textarea.setSelectionRange(index, indexAfterWord);
@@ -48,10 +48,10 @@ var app = new Vue({
             this.highlighted.indexAfterWord = indexAfterWord;
         }
     }
-  })
+})
 
 function getWordArray(words) {
-    return words.split(" ").filter(function(value) {
+    return words.split(" ").filter(function (value) {
         return value != "" && isNaN(value);
     });
 }
@@ -61,29 +61,49 @@ function getWordCount(wordArr) {
 }
 
 function getWordPairs(wordArr) {
-   var wordPairs = {};
-   wordArr.forEach(word => {
-       if (word in wordPairs) {
-           wordPairs[word] += 1;
-       }
-       else 
-       {
-           wordPairs[word] = 1;
-       }
-   });
+    var wordPairs = {};
+    wordArr.forEach(word => {
+        if (word in wordPairs) {
+            wordPairs[word] += 1;
+        }
+        else {
+            wordPairs[word] = 1;
+        }
+    });
 
-   return sortObjectByValueDescending(wordPairs);
+    var wordPairsSorted = sortObjectByValueDescending(wordPairs);
+
+    var wordPairArr = [];
+    var index = 0;
+    var rowLength = 0;
+    for (key in wordPairsSorted) {
+        rowLength += key.length;
+        if (rowLength > 23) {
+            index++;
+            rowLength = 0;
+        }
+
+        if (wordPairArr[index] == undefined) {
+            var obj = {};
+            obj[key] = wordPairsSorted[key];
+            wordPairArr.push(obj);
+        } else {
+            wordPairArr[index][key] = wordPairsSorted[key];
+        }
+    }
+
+    return wordPairArr;
 }
 
 function sortObjectByValueDescending(obj) {
     var keysSortedByValueArr = Object.keys(obj).sort(function (a, b) {
         return obj[b] - obj[a];
-   });
+    });
 
-   var objSorted = {};
-   for (key of keysSortedByValueArr) {
-    objSorted[key] = obj[key];
-   }
+    var objSorted = {};
+    for (key of keysSortedByValueArr) {
+        objSorted[key] = obj[key];
+    }
 
-   return objSorted;
+    return objSorted;
 }
